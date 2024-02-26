b2824830af5abea7fd66b593f0f69ba695b9a3ef
erquezada <erquezada@miners.utep.edu>
2024-02-26 01:08:29 -0700

    Updated history and main

--- src/uimain.c
+++ src/uimain.c
@@ -30,16 +30,16 @@ int main(){
     else if(userInput[0] == 'h'){
       print_history(history);
     }
-    else if(userInput[0] == '*'){
-      char *h = get_history(history, atoi(userInput + 1));
-      if(h != NULL){
-	char** tokens = tokenize(h);
-	printf("History: %s\n", h);
-	printf("Tokenized history:\n");
-	print_tokens(tokens);
-	free_tokens(tokens);
-      }else{
-      printf("Invalid history index\n");
+    else if (userInput[0] == '!') {
+      char *h = get_history(history, atoi(userInput + 1)); // prints a specific item
+      if (h != NULL) {
+        char** tokens = tokenize(h);
+        printf("History: %s\n", h);
+        printf("Tokenized history:\n");
+        print_tokens(tokens);
+        free_tokens(tokens);
+      } else {
+        printf("Invalid history index\n");
       }
     }
   }
